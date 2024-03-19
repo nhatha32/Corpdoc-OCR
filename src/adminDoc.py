b39d66cbf3c84a2ce9bd05e8e1421dbb76be02c9
nhatha32 <ng.nhatha32@gmail.com>
2024-03-19 20:15:25 +0700

    rename variable v2

--- src/adminDoc.py
+++ src/adminDoc.py
@@ -20,13 +20,13 @@ upperCharSet = (
 )
 
 
-# def splitText(text, startInit, endInit, objname, test):
-#     objval = text[startInit:endInit]
+# def splitText(raw_text, startInit, endInit, objname, test):
+#     objval = raw_text[startInit:endInit]
 #     test[objname] = objval
-#     return text[endInit + 1 :]
+#     return raw_text[endInit + 1 :]
 
 
-def postAdminDoc(text):
+def postAdminDoc(raw_text):
     ocrValInPage = {}
     # maxHeight = opencvImage.shape[0]
     # maxWidth = opencvImage.shape[1]
@@ -44,72 +44,72 @@ def postAdminDoc(text):
     # print(coQuan)
 
     # quoc hieu
-    # reg = re.search("cộng hòa xã hội chủ nghĩa việt nam", text)
+    # reg = re.search("cộng hòa xã hội chủ nghĩa việt nam", raw_text)
     # if reg != None:
-    #     text = splitText(text, reg.span()[0], reg.span()[1], "quoc_hieu", ocrValInPage)
+    #     raw_text = splitText(raw_text, reg.span()[0], reg.span()[1], "quoc_hieu", ocrValInPage)
 
     # tieu ngu
-    # reg = re.search("độc lập", text)
+    # reg = re.search("độc lập", raw_text)
 
     # if reg != None:
-    #     text = text[reg.span()[0] :]
+    #     raw_text = raw_text[reg.span()[0] :]
 
-    # reg = re.search(" hạnh phúc", text)
+    # reg = re.search(" hạnh phúc", raw_text)
 
     # if reg != None:
-    #     text = splitText(text, 0, reg.span()[1], "tieu_ngu", ocrValInPage)
+    #     raw_text = splitText(raw_text, 0, reg.span()[1], "tieu_ngu", ocrValInPage)
 
     # so hieu
-    # reg = re.search("số: ", text)
+    # reg = re.search("số: ", raw_text)
 
     # if reg != None:
-    #     text = text[reg.span()[1] :]
-    #     reg = re.search("\s", text)
+    #     raw_text = raw_text[reg.span()[1] :]
+    #     reg = re.search("\s", raw_text)
     #     if reg != None:
-    #         text = splitText(text, 0, reg.span()[0], "so", ocrValInPage)
+    #         raw_text = splitText(raw_text, 0, reg.span()[0], "so", ocrValInPage)
 
     # ocrValInPage["so"] = "06/tb-hcqt"
 
     # ngay thang
-    # reg = re.search("(" + lowerCharSet + "| )+, ngày .+ \d+", text)
+    # reg = re.search("(" + lowerCharSet + "| )+, ngày .+ \d+", raw_text)
 
     # if reg != None:
 
-    #     subText = text[reg.span()[0] : reg.span()[1]]
+    #     subText = raw_text[reg.span()[0] : reg.span()[1]]
 
     #     subObj = subText.split(", ")
 
     #     ocrValInPage["dia_diem"] = subObj[0]
     #     ocrValInPage["ngay_thang"] = subObj[1]
 
-    #     text = text[reg.span()[1] + 1 :]
+    #     raw_text = raw_text[reg.span()[1] + 1 :]
 
     # loai van ban
     regType = re.search(
         "(NGHỊ QUYẾT|NGHI QUYẾT|QUYẾT ĐỊNH|CHỈ THỊ|QUY CHẾ|QUY ĐỊNH|THÔNG CÁO|THONG CÁO|THÔNG BÁO|THONG BÁO|KẾ HOẠCH|HƯỚNG DẪN|HUỚNG DẪN|HƯÓNG DẪN|CHƯƠNG TRÌNH|PHƯƠNG ÁN|ĐỀ ÁN|DỰ ÁN|BÁO CÁO|BIÊN BẢN|TỜ TRÌNH|HỢP ĐỒNG|CÔNG VĂN|CÔNG ĐIỆN|BẢN GHI NHỚ|BẢN THỎA THUẬN|GIẤY ỦY QUYỀN|GIẤY MỜI|GIẤY GIỚI THIỆU|GIẤY NGHỈ PHÉP|PHIẾU GỬI|PHIẾU CHUYỂN|PHIẾU BÁO|THƯ CÔNG|THU CÔNG).*\n?",
-        text,
+        raw_text,
     )
 
     # if reg != None:
-    #     text = splitText(text, reg.span()[0], reg.span()[1] - 1, "loai_van_ban", ocrValInPage)
+    #     raw_text = splitText(raw_text, reg.span()[0], reg.span()[1] - 1, "loai_van_ban", ocrValInPage)
 
     # tieu de
-    # reg = re.search(lowerCharSet + ".*\n? | (căn cứ)", text)
-    reg = re.search("\n(Căn cứ|Kính gửi|Tại)", text)
+    # reg = re.search(lowerCharSet + ".*\n? | (căn cứ)", raw_text)
+    reg = re.search("\n(Căn cứ|Kính gửi|Tại)", raw_text)
 
     if regType and reg:
-        ocrValInPage["tieu_de"] = text[regType.span()[0] : reg.span()[0]]
+        ocrValInPage["tieu_de"] = raw_text[regType.span()[0] : reg.span()[0]]
         ocrValInPage["tieu_de"] = ocrValInPage["tieu_de"].replace("\n", " ")
 
     # các văn bản liên quan
-    # reg = re.search("(\d+/|)\d+/(" + lowerCharSet + "|-)+", text)
+    # reg = re.search("(\d+/|)\d+/(" + lowerCharSet + "|-)+", raw_text)
     # if reg != None:
     #     ocrValInPage["van_ban_lien_quan"] = ""
 
     # while reg != None:
-    #     objval = text[reg.span()[0] : reg.span()[1]]
+    #     objval = raw_text[reg.span()[0] : reg.span()[1]]
     #     ocrValInPage["van_ban_lien_quan"] += ocrValInPage["van_ban_lien_quan"] + "\n" + objval
-    #     text = text[reg.span()[1] :]
-    #     reg = re.search("(\d+/|)\d+/(" + lowerCharSet + "|-)+", text)
+    #     raw_text = raw_text[reg.span()[1] :]
+    #     reg = re.search("(\d+/|)\d+/(" + lowerCharSet + "|-)+", raw_text)
 
     return ocrValInPage
