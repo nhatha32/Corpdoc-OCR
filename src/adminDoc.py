c114124d08eacbd20fdd5f800de1d1c635ba14f2
nhatha32 <ng.nhatha32@gmail.com>
2024-03-19 20:13:37 +0700

    rename variable

--- src/adminDoc.py
+++ src/adminDoc.py
@@ -20,14 +20,14 @@ upperCharSet = (
 )
 
 
-def splitText(text, startInit, endInit, objname, test):
-    objval = text[startInit:endInit]
-    test[objname] = objval
-    return text[endInit + 1 :]
+# def splitText(text, startInit, endInit, objname, test):
+#     objval = text[startInit:endInit]
+#     test[objname] = objval
+#     return text[endInit + 1 :]
 
 
 def postAdminDoc(text):
-    test = {}
+    ocrValInPage = {}
     # maxHeight = opencvImage.shape[0]
     # maxWidth = opencvImage.shape[1]
 
@@ -39,14 +39,14 @@ def postAdminDoc(text):
     # cv2.waitKey(0)
     # coQuan = pytesseract.image_to_string(coQuanImg, lang="vie")
     # coQuan = chuan_hoa_dau_cau_tieng_viet(coQuan)
-    # test["co_quan"] = coQuan.replace("\n", " ", 1)
-    # test["co_quan"] = test["co_quan"].replace("\n", "", 1)
+    # ocrValInPage["co_quan"] = coQuan.replace("\n", " ", 1)
+    # ocrValInPage["co_quan"] = ocrValInPage["co_quan"].replace("\n", "", 1)
     # print(coQuan)
 
     # quoc hieu
     # reg = re.search("cộng hòa xã hội chủ nghĩa việt nam", text)
     # if reg != None:
-    #     text = splitText(text, reg.span()[0], reg.span()[1], "quoc_hieu", test)
+    #     text = splitText(text, reg.span()[0], reg.span()[1], "quoc_hieu", ocrValInPage)
 
     # tieu ngu
     # reg = re.search("độc lập", text)
@@ -57,7 +57,7 @@ def postAdminDoc(text):
     # reg = re.search(" hạnh phúc", text)
 
     # if reg != None:
-    #     text = splitText(text, 0, reg.span()[1], "tieu_ngu", test)
+    #     text = splitText(text, 0, reg.span()[1], "tieu_ngu", ocrValInPage)
 
     # so hieu
     # reg = re.search("số: ", text)
@@ -66,9 +66,9 @@ def postAdminDoc(text):
     #     text = text[reg.span()[1] :]
     #     reg = re.search("\s", text)
     #     if reg != None:
-    #         text = splitText(text, 0, reg.span()[0], "so", test)
+    #         text = splitText(text, 0, reg.span()[0], "so", ocrValInPage)
 
-    # test["so"] = "06/tb-hcqt"
+    # ocrValInPage["so"] = "06/tb-hcqt"
 
     # ngay thang
     # reg = re.search("(" + lowerCharSet + "| )+, ngày .+ \d+", text)
@@ -79,37 +79,37 @@ def postAdminDoc(text):
 
     #     subObj = subText.split(", ")
 
-    #     test["dia_diem"] = subObj[0]
-    #     test["ngay_thang"] = subObj[1]
+    #     ocrValInPage["dia_diem"] = subObj[0]
+    #     ocrValInPage["ngay_thang"] = subObj[1]
 
     #     text = text[reg.span()[1] + 1 :]
 
     # loai van ban
-    regLoai = re.search(
+    regType = re.search(
         "(NGHỊ QUYẾT|NGHI QUYẾT|QUYẾT ĐỊNH|CHỈ THỊ|QUY CHẾ|QUY ĐỊNH|THÔNG CÁO|THONG CÁO|THÔNG BÁO|THONG BÁO|KẾ HOẠCH|HƯỚNG DẪN|HUỚNG DẪN|HƯÓNG DẪN|CHƯƠNG TRÌNH|PHƯƠNG ÁN|ĐỀ ÁN|DỰ ÁN|BÁO CÁO|BIÊN BẢN|TỜ TRÌNH|HỢP ĐỒNG|CÔNG VĂN|CÔNG ĐIỆN|BẢN GHI NHỚ|BẢN THỎA THUẬN|GIẤY ỦY QUYỀN|GIẤY MỜI|GIẤY GIỚI THIỆU|GIẤY NGHỈ PHÉP|PHIẾU GỬI|PHIẾU CHUYỂN|PHIẾU BÁO|THƯ CÔNG|THU CÔNG).*\n?",
         text,
     )
 
     # if reg != None:
-    #     text = splitText(text, reg.span()[0], reg.span()[1] - 1, "loai_van_ban", test)
+    #     text = splitText(text, reg.span()[0], reg.span()[1] - 1, "loai_van_ban", ocrValInPage)
 
     # tieu de
     # reg = re.search(lowerCharSet + ".*\n? | (căn cứ)", text)
     reg = re.search("\n(Căn cứ|Kính gửi|Tại)", text)
 
-    if regLoai and reg:
-        test["tieu_de"] = text[regLoai.span()[0] : reg.span()[0]]
-        test["tieu_de"] = test["tieu_de"].replace("\n", " ")
+    if regType and reg:
+        ocrValInPage["tieu_de"] = text[regType.span()[0] : reg.span()[0]]
+        ocrValInPage["tieu_de"] = ocrValInPage["tieu_de"].replace("\n", " ")
 
     # các văn bản liên quan
     # reg = re.search("(\d+/|)\d+/(" + lowerCharSet + "|-)+", text)
     # if reg != None:
-    #     test["van_ban_lien_quan"] = ""
+    #     ocrValInPage["van_ban_lien_quan"] = ""
 
     # while reg != None:
     #     objval = text[reg.span()[0] : reg.span()[1]]
-    #     test["van_ban_lien_quan"] += test["van_ban_lien_quan"] + "\n" + objval
+    #     ocrValInPage["van_ban_lien_quan"] += ocrValInPage["van_ban_lien_quan"] + "\n" + objval
     #     text = text[reg.span()[1] :]
     #     reg = re.search("(\d+/|)\d+/(" + lowerCharSet + "|-)+", text)
 
-    return test
+    return ocrValInPage
--- src/book.py
+++ src/book.py
@@ -13,38 +13,38 @@ upperCharSet = (
 )
 
 
-def splitText(text, startInit, endInit, objname, test):
+def splitText(text, startInit, endInit, objname, ocrValInPage):
     objval = text[startInit:endInit]
-    test[objname] = objval
+    ocrValInPage[objname] = objval
     return text[endInit + 1 :]
 
 
 def postBook(raw_text):
-    test = {}
-    test["body"] = raw_text[0:700]
+    ocrValInPage = {}
+    ocrValInPage["body"] = raw_text[0:700]
     reg = re.search("isbn|ISBN|Isbn", raw_text)
     if reg != None:
         x = raw_text.split("\n")
         for j in x:
             reg = re.search("isbn|ISBN|Isbn", j)
             if reg != None:
-                test["isbn"] = j[reg.span()[1] :]
+                ocrValInPage["isbn"] = j[reg.span()[1] :]
 
     reg = re.search("NHÀ XUẤT BẢN", raw_text)
-    if "nxb" not in test and reg != None:
+    if "nxb" not in ocrValInPage and reg != None:
         x = raw_text.split("\n")
         for j in x:
             reg = re.search("NHÀ XUẤT BẢN", j)
             if reg != None:
-                test["nxb"] = j
+                ocrValInPage["nxb"] = j
 
     reg = re.search("NXB", raw_text)
-    if "nxb" not in test and reg != None:
+    if "nxb" not in ocrValInPage and reg != None:
         x = raw_text.split("\n")
         for j in x:
             reg = re.search("NXB", j)
             if reg != None:
-                test["nxb"] = j
+                ocrValInPage["nxb"] = j
 
     reg = re.search("trách nhiệm xuất bản", raw_text)
 
@@ -52,31 +52,31 @@ def postBook(raw_text):
 
         x = raw_text.split("\n")
         for j in x:
-            if "title" not in test and j == j.upper():
-                test["title"] = j
+            if "title" not in ocrValInPage and j == j.upper():
+                ocrValInPage["title"] = j
                 continue
-            if "author" not in test and j == j.upper():
-                test["author"] = j
+            if "author" not in ocrValInPage and j == j.upper():
+                ocrValInPage["author"] = j
                 continue
             reg = re.search("NHÀ XUẤT BẢN", j)
-            if "nxb" not in test and reg != None:
-                test["nxb"] = j
+            if "nxb" not in ocrValInPage and reg != None:
+                ocrValInPage["nxb"] = j
                 continue
             reg = re.search("NXB", j)
-            if "nxb" not in test and reg != None:
-                test["nxb"] = j
+            if "nxb" not in ocrValInPage and reg != None:
+                ocrValInPage["nxb"] = j
                 continue
             reg = re.search("Người dịch", j)
             if reg != None:
-                test["nguoi_dich"] = j[reg.span()[1] + 1 :]
+                ocrValInPage["nguoi_dich"] = j[reg.span()[1] + 1 :]
                 continue
             reg = re.search("Email", j)
             if reg != None:
-                test["email"] = j[reg.span()[1] + 1 :]
+                ocrValInPage["email"] = j[reg.span()[1] + 1 :]
                 continue
             reg = re.search("isbn", j)
             if reg != None:
-                test["isbn"] = j[reg.span()[1] :]
+                ocrValInPage["isbn"] = j[reg.span()[1] :]
                 continue
 
-    return test
+    return ocrValInPage
